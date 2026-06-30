0925324c36ec58ae1294e4f2b7492a4af3612ee4
Эмиль Абдуллаев <emil2008abdullaev@gmail.com>
2026-06-30 22:59:43 +0300

    Добавьте файлы проекта.

---
+++ Facade.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Facade
+    {
+        private Random random = new Random();
+        public List<int> GenerateNumbers(int count) 
+        {
+            List<int> numbers = new List<int>();
+            for (int i= 0; i < count; i++) 
+            {
+                numbers.Add(random.Next(0,101));
+            }
+            return numbers;
+        }
+        public async Task ShowNumbersAsync(List<int> numbers, double secondsPerNumber, Action<string> updateUiAction) 
+        {
+            foreach (int num in numbers)
+            {
+                updateUiAction(num.ToString());
+                await Task.Delay(TimeSpan.FromSeconds(secondsPerNumber));
+                updateUiAction("");
+                await Task.Delay(200);
+            }
+        }
+
+        public (double coefficient, int correctCount) CalculateResult(List<int> generated, string userInput) 
+        {
+            string[] splirText = userInput.Split(',', ' ');
+            List<int> userNumbers = new List<int>();
+            foreach (string s in splirText) 
+            {
+                if (Int32.TryParse(s, out int val))
+                {
+                    userNumbers.Add(val);
+                }
+                else 
+                {
+                    userNumbers.Add(-1); 
+                }
+            }
+            int correctCount = 0;
+            for (int i=0; i < Math.Min(generated.Count, userNumbers.Count); i++) 
+            {
+                if (generated[i] == userNumbers[i]) 
+                {
+                    correctCount++;
+                }
+            }
+            double coefficient = (double)correctCount / 5.0;
+            return (coefficient, correctCount);
+        }
+    }
+}
